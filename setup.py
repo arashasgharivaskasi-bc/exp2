0abccc942925920bebd8633c73f0f7c6d000e18c
Your Name <you@example.com>
2019-04-09 13:17:02 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='exp2',
+    version='0.0.1',
+    description='Brain Corp experimental exploration project',
+    long_description=long_description,
+    author='Arash Asgharivaskasi',
+    author_email='c_asgharivaskasi@braincorporation.com',
+    url='https://github.com/arashasgharivaskasi-bc/exp2',
+    download_url='',
+    license='Braincorp',
+    install_requires=['numpy>=1.13.3',
+                      'opencv-python'],
+        extras_require={
+        'tests': ['pytest==4.3.0',
+                  'pytest-pep8==1.0.6',
+                  'pytest-xdist==1.26.1',
+                  'pylint==1.9.2',
+                  'astroid==1.6.5'
+                  ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=find_packages(),
+)
+
